ce88ee15bf4e9fb27161ae8654b8841c32a6340a
Meash <m4rtinsch@gmail.com>
2015-04-20 11:21:22 +0200

    [BROKEN] Write sorted chunks to temp files and read first bytes in merge phase.

--- src/assignment1/external_sort.cpp
+++ src/assignment1/external_sort.cpp
@@ -6,6 +6,7 @@
 #include <io.h>
 #include <stdio.h>
 #include <algorithm>
+#include <fcntl.h>
 #include "external_sort.h"
 
 uint64_t div_ceil(uint64_t divisor, uint64_t dividend);
@@ -15,6 +16,7 @@ void external_sort(int fdInput, uint64_t size, int fdOutput, uint64_t memSize /*
 	uint64_t chunk[memSize];
 	printf("Chunk phase with %llu chunks\n", chunks);
 	// steps 1 - 3
+	int fdsTemp[chunks];
 	for (unsigned int i(0); i < chunks; i++) {
 		// read chunk
 		int r = read(fdInput, chunk, sizeof(chunk));
@@ -25,7 +27,18 @@ void external_sort(int fdInput, uint64_t size, int fdOutput, uint64_t memSize /*
 		// sort chunk
 		std::sort(std::begin(chunk), std::end(chunk));
 		// write chunk to output file
-		int w = write(fdOutput, chunk, sizeof(chunk));
+		// IDEA: could write only to one file (e.g. output file) with offsets
+		char iteratorString[21];
+		sprintf(iteratorString, "%d", i);
+		std::string tempFile = "temp-";
+		tempFile = tempFile + iteratorString;
+		fdsTemp[i] = open(tempFile.c_str(), O_CREAT | O_TRUNC | O_WRONLY);
+		if(fdsTemp[i] < 0) {
+			std::string msg = "Cannot open temp file";
+			msg = msg + tempFile;
+			perror(msg.c_str());
+		}
+		int w = write(fdsTemp[i], chunk, sizeof(chunk));
 		if (w < 0) {
 			perror("Could not write chunk");
 			return;
@@ -39,26 +52,19 @@ void external_sort(int fdInput, uint64_t size, int fdOutput, uint64_t memSize /*
 	uint64_t outputBuffer[bufferSize];
 
 	// read data in buffer
-
-
-	// read
-	uint64_t input[size];
-	int r = read(fdInput, input, sizeof(input));
-	if (r < 0) {
-		perror("Read error");
-		return;
+	for (unsigned int i(0); i < chunks; i++) {
+		int fd = fdsTemp[i];
+		read(fd, inputBuffers + i * bufferSize, (int) bufferSize);
 	}
 
-	// sort
-	size_t length = sizeof(input) / sizeof(uint64_t);
-	for (unsigned int i(0); i < length; ++i) {
-		uint64_t data = input[i];
-	}
-	int w = write(fdOutput, input, sizeof(input));
-	if (w < 0) {
-		perror("Could not write data");
-		return;
-	}
+
+
+
+//	int w = write(fdOutput, input, sizeof(input));
+//	if (w < 0) {
+//		perror("Could not write data");
+//		return;
+//	}
 
 
 	close(fdInput);
