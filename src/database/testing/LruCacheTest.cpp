4173d54d52f48a53dc64ed704607e470e4ad596f
Daniel Langerenken <daniel.langerenken@gmail.com>
2015-04-27 15:27:44 +0200

    fixed a bit form lrucache, added a todo and started to write tests

--- src/database/testing/LruCacheTest.cpp
+++ src/database/testing/LruCacheTest.cpp
@@ -8,5 +8,13 @@
 int main(){
     LruCache<std::string, std::string> cache (5);
     cache.put("Test","Test1");
-    printf("Cache size %l", cache.size());
+    printf("Cache size %zu\n", cache.size());
+    cache.put("Test2", "Test2");
+    cache.put("Test3", "Test2");
+    cache.put("Test4", "Test2");
+    cache.put("Test5", "Test2");
+    cache.put("Test6", "Test2");
+    printf("Cache size %zu", cache.size());
+    printf("Should not contain value: %zu", cache.contains("Test"));
+    printf("Should contain value: %zu", cache.contains("Test2"));
 }
--- src/database/util/LruCache.h
+++ src/database/util/LruCache.h
@@ -14,23 +14,24 @@
  * and https://github.com/lamerman/cpp-lru-cache/blob/master/include/lrucache.hpp */
 template<typename Key, typename Value>
 class LruCache {
+    typedef std::pair<Key , Value> Pair;
+    typedef std::list<Pair> CacheList;
+    typedef std::unordered_map<Key, Value> CacheMap;
 
 public:
+    CacheList cacheList;
+    CacheMap cacheMap;
+    uint64_t entries = 0;
+    size_t maxEntries;
+
     LruCache(size_t maxEntries);
     ~LruCache();
     void put(Key key, Value value);
     Value get(Key key);
-    uint64_t size();
-
-private:
-    typedef std::unordered_map<Key, Value> CacheMap;
-    CacheMap cacheMap;
-    typedef std::pair<Key , Value> Pair;
-    typedef std::list<Pair> CacheList;
-    CacheList cacheList;
-    uint64_t entries = 0;
-    size_t maxEntries;
+    size_t size();
+    bool contains(Key key);
 };
 
+#include "../util/LruCache.cpp"
 
 #endif //PROJECT_LRUCACHE_H
