f24639e1ea0181845c1c9c74ebd795191ba01696
Meash <m4rtinsch@gmail.com>
2015-04-29 23:59:26 +0200

    Implement getters and setters.

--- src/database/buffer/BufferFrame.cpp
+++ src/database/buffer/BufferFrame.cpp
@@ -1,39 +1,64 @@
 #include <stdint.h>
 #include "BufferFrame.hpp"
 
+BufferFrame::BufferFrame(uint64_t pageId, uint64_t segmentId, void *data) {
+	this->pageId = pageId;
+	this->segmentId = segmentId;
+	this->data = data;
+}
+
 void *BufferFrame::getData() {
 	return this->data;
 }
 
-
 uint64_t BufferFrame::getPageId() {
-
+	return this->pageId;
 }
 
 uint64_t BufferFrame::getSegmentId() {
+	return this->segmentId;
+}
 
+void BufferFrame::setDirty(bool dirty) {
+	this->setFlagBool(dirty, DIRTY_FLAG);
 }
 
 bool BufferFrame::isDirty() {
-	return this->isFlagSet(DIRTY_MASK);
+	return this->isFlagSet(DIRTY_FLAG);
+}
+
+void BufferFrame::setExclusive(bool exclusive) {
+	this->setFlagBool(exclusive, EXCLUSIVE_FLAG);
 }
 
 bool BufferFrame::isExclusive() {
-	return this->isFlagSet(EXCLUSIVE_MASK);
+	return this->isFlagSet(EXCLUSIVE_FLAG);
+}
+
+void BufferFrame::setUnfixed(bool unfixed) {
+	this->setFlagBool(unfixed, UNFIXED_FLAG);
 }
 
 bool BufferFrame::isUnfixed() {
-	return this->isFlagSet(UNFIXED_MASK);
+	return this->isFlagSet(UNFIXED_FLAG);
 }
 
 bool BufferFrame::isFlagSet(uint8_t mask) {
 	return (this->state & mask) == mask;
 }
 
-void BufferFrame::setFlag(uint8_t mask) {
-	this->state |= mask;
+void BufferFrame::setFlagBool(bool flagSet, uint8_t flag) {
+	if(flagSet) {
+		this->setFlag(flag);
+	} else {
+		this->unsetFlag(flag);
+	}
 }
 
-void BufferFrame::unsetFlag(uint8_t mask) {
-	this->state &= ~mask;
+void BufferFrame::setFlag(uint8_t flag) {
+	this->state |= flag;
+}
+
+void BufferFrame::unsetFlag(uint8_t flag) {
+	this->state &= ~flag;
 }
--- src/database/buffer/BufferFrame.hpp
+++ src/database/buffer/BufferFrame.hpp
@@ -2,30 +2,39 @@
 #define PROJECT_BUFFER_FRAME_H
 
 class BufferFrame {
-	const uint8_t DIRTY_MASK = 0x1; // 001
-	const uint8_t EXCLUSIVE_MASK = 0x2; // 010
-	const uint8_t UNFIXED_MASK = 0x4; // 100
+	const uint8_t DIRTY_FLAG = 0x1; // 001
+	const uint8_t EXCLUSIVE_FLAG = 0x2; // 010
+	const uint8_t UNFIXED_FLAG = 0x4; // 100
 
 private:
 	void *data;
 	uint64_t pageId;
+	uint64_t segmentId;
 	uint8_t state; // combine dirty and exclusive flag - see chapter 2, slide 17
 	boost::shared_mutex latch; // TODO: make sure false conflicts are avoided - modulo(sizeof(BufferFrame), cache_line_size_byte = 64) = 0!
 
 	bool isFlagSet(uint8_t mask);
 
-	void setFlag(uint8_t mask);
+	void setFlagBool(bool flagSet, uint8_t flag);
 
-	void unsetFlag(uint8_t mask);
+	void setFlag(uint8_t flag);
+
+	void unsetFlag(uint8_t flag);
 
 public:
-	BufferFrame(uint64_t pageId, void *page);
+	BufferFrame(uint64_t pageId, uint64_t segmentId, void *data);
 
 	void *getData();
 
+	void setDirty(bool dirty);
+
 	bool isDirty();
 
+	void setExclusive(bool exclusive);
+
 	bool isExclusive();
+
+	void setUnfixed(bool unfixed);
 
 	bool isUnfixed();
 
