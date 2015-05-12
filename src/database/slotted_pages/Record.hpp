79a04584bcaa182e251ef1eb513a7b430e5ed83d
Meash <m4rtinsch@gmail.com>
2015-05-12 12:28:37 +0200

    Do not free nullptr. Rename TID.

--- src/database/slotted_pages/Record.hpp
+++ src/database/slotted_pages/Record.hpp
@@ -6,47 +6,55 @@
 
 // A simple Record implementation
 class Record {
-   unsigned len;
-   char* data;
+	unsigned len;
+	char *data;
 
 public:
-   // Assignment Operator: deleted
-   Record& operator=(Record& rhs) = delete;
-   // Copy Constructor: deleted
-   Record(Record& t) = delete;
-   // Move Constructor
-   Record(Record&& t);
-   // Constructor
-   Record(unsigned len, const char* const ptr);
-   // Destructor
-   ~Record();
-   // Get pointer to data
-   const char* getData() const;
-   // Get data size in bytes
-   unsigned getLen() const;
+	// Assignment Operator: deleted
+	Record &operator=(Record &rhs) = delete;
+
+	// Copy Constructor: deleted
+	Record(Record &t) = delete;
+
+	// Move Constructor
+	Record(Record &&t);
+
+	// Constructor
+	Record(unsigned len, const char *const ptr);
+
+	// Destructor
+	~Record();
+
+	// Get pointer to data
+	const char *getData() const;
+
+	// Get data size in bytes
+	unsigned getLen() const;
 };
 
-Record::Record(Record&& t) : len(t.len), data(t.data) {
-   t.data = nullptr;
-   t.len = 0;
+Record::Record(Record &&t) : len(t.len), data(t.data) {
+	t.data = nullptr;
+	t.len = 0;
 }
 
-Record::Record(unsigned len, const char* const ptr) : len(len) {
-   data = static_cast<char*>(malloc(len));
-   if (data)
-      memcpy(data, ptr, len);
+Record::Record(unsigned len, const char *const ptr) : len(len) {
+	data = static_cast<char *>(malloc(len));
+	if (data)
+		memcpy(data, ptr, len);
 }
 
-const char* Record::getData() const {
-   return data;
+const char *Record::getData() const {
+	return data;
 }
 
 unsigned Record::getLen() const {
-   return len;
+	return len;
 }
 
 Record::~Record() {
-   free(data);
+	if (data != nullptr) {
+		free(data);
+	}
 }
 
 #endif
