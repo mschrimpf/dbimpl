299c0d6887da751799fe4ddcd0f58699e67c8a52
Meash <m4rtinsch@gmail.com>
2015-05-12 12:16:34 +0200

    Split up structs. Implement lookup.

--- testing/slotted_pages/unit_tests.cpp
+++ testing/slotted_pages/unit_tests.cpp
@@ -4,9 +4,14 @@
 
 #include "gtest/gtest.h"
 #include "../../src/database/slotted_pages/SlottedPage.hpp"
+#include "../../src/database/slotted_pages/SPSegment.hpp"
 
 TEST(SlottedPageTest, SizeMatchesPageSize) {
 	EXPECT_EQ(PAGE_SIZE_BYTE, sizeof(SlottedPage));
+}
+
+TEST(SlotDataTest, LengthSizeMatchesSlotLengthSize) {
+	EXPECT_EQ(sizeof(SlottedPage::Slot::length), sizeof(SPSegment::SlotData::length));
 }
 
 int main(int argc, char **argv) {
