a35f60933e075c5d18f3b3d3f6510f226413b47a
Daniel Langerenken <daniel.langerenken@gmail.com>
2015-05-13 13:10:44 +0200

    added schema-serializer test

---
+++ src/database/relations/SchemaSerializer.cpp
@@ -0,0 +1,29 @@
+//
+// Created by daniel on 5/13/15.
+//
+
+#include <sstream>
+#include "SchemaSerializer.h"
+
+Schema* loadSchema() {
+
+}
+
+//  RELATIONS_COUNT
+//  RELATION1_SIZE
+//  RELATION1_NAME
+//  RELATION1_COUNT_ATTRIBUTES
+//      ATTRIBUTE_NAME (
+//      ATTRIBUTE_TYPE (0 INT, 1 CHAR)
+//      ATTRIBUTE_LEN
+//      ATTRIBUTE_NOTNULL
+//  ....
+//  RELATION1.COUNT_PRIMARY_KEYS
+//      PRIMARY_KEY1
+//      ...
+//
+void storeSchema(Schema* schema) {
+
+}
+
+//void write();
---
+++ src/database/relations/SchemaSerializer.h
@@ -0,0 +1,13 @@
+//
+// Created by daniel on 5/13/15.
+//
+
+#ifndef PROJECT_SCHEMASERIALIZER_H
+#define PROJECT_SCHEMASERIALIZER_H
+
+#include "Schema.hpp"
+
+void storeSchema(Schema* schema);
+Schema* loadSchema();
+
+#endif //PROJECT_SCHEMASERIALIZER_H
