69632bc9cd469a68dcdd463d707a6607f943bf43
Meash <m4rtinsch@gmail.com>
2015-06-18 16:51:44 +0200

    Add tests for scanning strings from table.

--- testing/operators/tablescan_operator_tests.cpp
+++ testing/operators/tablescan_operator_tests.cpp
@@ -67,6 +67,47 @@ public:
 
     op.close();
   }
+
+  void testString(const unsigned numRecords, const unsigned numColumns, const unsigned numChars) {
+    unsigned strLen = numChars * sizeof(char);
+    char values[numRecords][numColumns][strLen];
+    size_t recordLen = numColumns * strLen;
+
+    char valueIt = '1';
+    for (unsigned r = 0; r < numRecords; r++) {
+      for (unsigned c = 0; c < numColumns; c++) {
+        for (int s = 0; s < strLen; s++) {
+          values[r][c][s] = valueIt;
+        }
+      }
+      valueIt++;
+      segment->insert(Record(recordLen, (const char *) values[r]));
+    }
+
+    Schema::Relation relation("Pairs");
+    for (unsigned c = 0; c < numColumns; c++) {
+      std::string colString = std::to_string(c);
+      Schema::Relation::Attribute attr("val-" + colString, Types::Tag::Char, strLen);
+      relation.addAttribute(attr);
+    }
+
+    TableScanOperator op(relation, *segment);
+    op.open();
+    for (unsigned r = 0; r < numRecords; r++) {
+      ASSERT_TRUE(op.next());
+      ASSERT_EQ(numColumns, op.getOutput().size());
+      for (unsigned c = 0; c < numColumns; c++) {
+        Register *reg = op.getOutput()[c];
+        const char *regStr = reg->getStringValue().c_str();
+        for (unsigned s = 0; s < strLen; s++) {
+          ASSERT_EQ(values[r][c][s], regStr[s]);
+        }
+      }
+    }
+    ASSERT_FALSE(op.next());
+
+    op.close();
+  }
 };
 
 TEST_F(TableScanOperatorTest, IntegerOneColumn) {
@@ -75,4 +116,16 @@ TEST_F(TableScanOperatorTest, IntegerOneColumn) {
 
 TEST_F(TableScanOperatorTest, IntegerTwoColumns) {
   testInteger(3, 2);
+}
+
+TEST_F(TableScanOperatorTest, StringOneColumnOneChar) {
+  testString(3, 1, 1);
+}
+
+TEST_F(TableScanOperatorTest, StringTwoColumnsOneChar) {
+  testString(3, 2, 1);
+}
+
+TEST_F(TableScanOperatorTest, StringOneColumnThreeChars) {
+  testString(3, 1, 3);
 }
