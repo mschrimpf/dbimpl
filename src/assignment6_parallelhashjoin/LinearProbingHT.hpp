d6322a3d177e817d672f41516395dd50dd51c2f3
Daniel Langerenken <daniel.langerenken@gmail.com>
2015-06-23 16:06:33 +0200

    added linearprobing without concurrency

--- src/assignment6_parallelhashjoin/LinearProbingHT.hpp
+++ src/assignment6_parallelhashjoin/LinearProbingHT.hpp
@@ -3,9 +3,14 @@
 
 #include <atomic>
 #include "stdint.h"
+#include "hashjoinskeleton.cpp"
 
 class LinearProbingHT {
+private:
+  Entry *entries;
+  uint64_t size;
 public:
+
   // Entry
   struct Entry {
     uint64_t key;
@@ -14,17 +19,51 @@ public:
   };
 
   // Constructor
-  LinearProbingHT(uint64_t size) {
+  LinearProbingHT(uint64_t size) : size(size) {
+    entries = new Entry[size];
   }
 
   // Destructor
   ~LinearProbingHT() {
+    delete[] entries;
   }
 
   inline Entry *lookup(uint64_t key) {
+    uint64_t hash = hashKey(key);
+    for (uint64_t i = 0; i < size; i++) {
+      Entry *entry = &entries[hash % size];
+      if (entry->key == key) {
+        //correct position found
+        if (entry->marker) {
+          // value set-> return entry
+          return entry;
+        } else {
+          //value not set -> return nothing
+          return nullptr;
+        }
+      } else {
+        if (!entry->marker) {
+          //empty spot available -> our entry is not inside of the map
+          return nullptr;
+        }
+      }
+    }
+    // no key matched -> no value found
+    return nullptr;
   }
 
-  inline void insert(uint64_t key) {
+  inline void insert(uint64_t key, uint64_t value) {
+    uint64_t hash = hashKey(key);
+    for (uint64_t i = 0; i < size; i++) {
+      Entry *entry = &entries[hash % size];
+      if (!entry->marker) {
+        //value added
+        entry->key = key;
+        entry->value = value;
+        entry->marker = true;
+      }
+    }
+    //value not added
   }
 };
 
