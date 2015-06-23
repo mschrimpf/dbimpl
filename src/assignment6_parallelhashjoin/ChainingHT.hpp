9e36e144547fa97e8c80e9ab077d3208e49ffb51
Meash <m4rtinsch@gmail.com>
2015-06-23 15:15:54 +0200

    Implement ChainingHT.

--- src/assignment6_parallelhashjoin/ChainingHT.hpp
+++ src/assignment6_parallelhashjoin/ChainingHT.hpp
@@ -1,7 +1,13 @@
 #ifndef PROJECT_CHAININGHT_H
 #define PROJECT_CHAININGHT_H
 
+#include "hashjoinskeleton.cpp"
+#include <atomic>
+
 class ChainingHT {
+private:
+  Entry *entries;
+
 public:
   // Chained tuple entry
   struct Entry {
@@ -12,16 +18,30 @@ public:
 
   // Constructor
   ChainingHT(uint64_t size) {
+    entries = new Entry[size];
   }
 
   // Destructor
   ~ChainingHT() {
+    delete[] entries;
   }
 
   inline Entry *lookup(uint64_t key) {
+    uint64_t hash = hashKey(key);
+    return entries[hash];
   }
 
   inline void insert(Entry *entry) {
+    uint64_t key = entry->key;
+    uint64_t val = entry->value;
+    Entry newEntry;
+    newEntry.key = key;
+    newEntry.value = val;
+    uint64_t hash = hashKey(key);
+
+    do {
+      newEntry.next = entries[hash];
+    } while (compare_exchange_strong(entries[hash], newEntry, std::memory_order_relaxed));
   }
 };
 
