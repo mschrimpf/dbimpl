441d0f9ca82fcd98e23c3a18f8947e3fa2847f26
Meash <m4rtinsch@gmail.com>
2015-07-07 08:21:23 +0200

    Add ChainingLockingCachelineHT.

---
+++ src/assignment6_parallelhashjoin/ChainingLockingCachelineHT.cpp
@@ -0,0 +1,85 @@
+#ifndef PROJECT_CHAININGLOCKINGCACHELINEHT_H
+#define PROJECT_CHAININGLOCKINGCACHELINEHT_H
+
+#include "hash.hpp"
+#include "stdint.h"
+#include "inttypes_wrapper.hpp"
+#include <mutex>
+#include <stdio.h>
+
+class ChainingLockingCachelineHT {
+public:
+  // Chained tuple entry
+  struct Entry {
+    uint64_t key;
+    Entry *next;
+
+    Entry(uint64_t key) : key(key), next(nullptr) { }
+
+    Entry() { }
+  };
+
+private:
+  struct Bucket {
+    Entry *entry;
+    std::mutex lock;
+
+    Bucket() : entry(nullptr) { }
+  };
+
+  Bucket *buckets;
+  uint64_t size;
+
+public:
+  // Constructor
+  ChainingLockingCachelineHT(uint64_t size) : size(size) {
+    buckets = new Bucket[size];
+    for (uint64_t i = 0; i < size; i++) {
+      buckets[i].entry = nullptr;
+    }
+  }
+
+  // Destructor
+  ~ChainingLockingCachelineHT() {
+    delete[] buckets;
+  }
+
+  // Returns the number of hits
+  inline uint64_t lookup(uint64_t key) const {
+    uint64_t hash = hashKey(key) % size;
+    Entry *entry = buckets[hash].entry;
+    uint64_t count = 0;
+    while (entry != nullptr) {
+      if (entry->key == key) {
+        count++;
+      }
+      entry = entry->next;
+    }
+    return count;
+  }
+
+  inline void insert(Entry *newEntry) {
+    uint64_t hash = hashKey(newEntry->key) % size;
+    std::lock_guard<std::mutex> lock(buckets[hash].lock);
+    Entry *currHead = buckets[hash].entry;
+    buckets[hash].entry = newEntry;
+    newEntry->next = currHead;
+  }
+
+  inline void print() {
+    for (unsigned i = 0; i < size; i++) {
+      Entry *entry = buckets[i].entry;
+      if (entry == nullptr) {
+        continue;
+      }
+      printf("[%u]", i);
+      while (entry != nullptr) {
+        printf(" -> (%" PRIu64 ")", entry->key);
+        entry = entry->next;
+      }
+      printf("\n");
+    }
+  }
+};
+
+#endif //CACHELINE
